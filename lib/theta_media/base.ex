e250b8c8e36d03b7ee5cefd334af23361012e9ca
Lang Pham <phamxuanlang@gmail.com>
2020-07-01 14:04:28 +0700

    Dir and Base

--- lib/theta_media/base.ex
+++ lib/theta_media/base.ex
@@ -1,26 +1,89 @@
 defmodule ThetaMedia.Base do
-  @moduledoc """
-    pwd: "uploads"
-    dirs: [dirA,dirB]
-    files: [file1, file2]
-  """
-  defstruct base: "",
-            dirs: [],
-            files: []
+	@moduledoc """
+	  base: "uploads"
+		root: 0
+	  dirs: [dirA,dirB]
+	  files: [file1, file2]
+	"""
+	defstruct base: "",
+	          root: 1,
+	          dirs: [],
+	          files: []
 
-  @doc """
-  Returns the list of school.
+	@doc """
+	Returns the object of Base.
 
-  ## Examples
+	## Examples
 
-      iex> new()
-      [%School{}, ...]
+	    iex> new()
+	    %ThetaMedia.Base{
+	      base: "uploads"
+				root: 1
+	      dirs: []
+	      files: []
+	    }
+	"""
+	def new() do
+		store = Application.get_env(:theta_media, :storage)
+		IO.inspect store
+		IO.inspect Path.split(store)
+		root = Enum.count(Path.split(store))
+		IO.inspect root
+		this = put_in(%__MODULE__{}.root, root)
+		put_in(this.base, store)
+	end
 
-  """
-  def new() do
-    store = Application.get_env(:theta_media, :storage)
-    put_in(%__MODULE__{}.base, store)
-  end
+	@doc """
+	Returns the object of Base with change base.
 
+	## Examples
+
+	    iex> into(string)
+	    %ThetaMedia.Base{
+	      base: "uploads/string"
+				root: 1
+	      dirs: []
+	      files: []
+	    }
+	"""
+	def into(%__MODULE__{} = base, string) do
+		IO.inspect string, label: "String:\n"
+		IO.inspect base.base, label: "Base:\n"
+		path = base.base
+		base = put_in(base.base, Path.join(base.base, string))
+	end
+
+	@doc """
+	Returns the object of Base with change base.
+
+	## Examples
+			base = %ThetaMedia.Base{
+	      base: "uploads/string"
+				root: 1
+	      dirs: []
+	      files: []
+	    }
+
+	    iex> outgo(base)
+	    %ThetaMedia.Base{
+	      base: "uploads"
+				root: 1
+	      dirs: []
+	      files: []
+	    }
+	"""
+	def outgo(%__MODULE__{} = base) do
+		list = Path.split(base.base)
+		{_,list_new} =
+			if Enum.count(list) > base.root do
+				List.pop_at(list, -1)
+			else
+				{ nil,list}
+			end
+
+		#		IO.inspect base.base, label: "Base:\n"
+		#		path = base.base
+		put_in(base.base, Path.join(list_new))
+	end
 
 end
---
+++ lib/theta_media/dir.ex
@@ -0,0 +1,25 @@
+defmodule ThetaMedia.Dir do
+	alias ThetaMedia.Base
+	def list() do
+		store = Application.get_env(:theta_media, :storage)
+		base = Base.new()
+		base = put_in(base.base, store)
+		list = File.ls!(store)
+		dirs = Enum.filter(list, fn (x) -> File.stat!(Path.join(store, x)).type == :directory end)
+		base = put_in(base.dirs, dirs)
+		files = Enum.filter(list, fn (x) -> File.stat!(Path.join(store, x)).type != :directory end)
+		base = put_in(base.files, files)
+		base
+	end
+
+	def list(%Base{} = base) do
+		store = base.base
+		list = File.ls!(store)
+		dirs = Enum.filter(list, fn (x) -> File.stat!(Path.join(store, x)).type == :directory end)
+		base = put_in(base.dirs, dirs)
+		files = Enum.filter(list, fn (x) -> File.stat!(Path.join(store, x)).type != :directory end)
+		base = put_in(base.files, files)
+		base
+	end
+
+end
