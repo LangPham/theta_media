859472b0766a702ea6b7b17981b48322d737fbf9
Lang Pham <phamxuanlang@gmail.com>
2020-07-01 18:16:34 +0700

    Delete cratf

--- lib/theta_media.ex
+++ lib/theta_media.ex
@@ -3,22 +3,10 @@ defmodule ThetaMedia do
   Documentation for `ThetaMedia`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ThetaMedia.hello()
-      :world
-
-  """
-  def hello do
-    store = Application.get_env(:theta_media, :storage)
-    list = File.ls!(store)
-    dirs = Enum.filter(list, fn(x) -> File.stat!(Path.join(store,x)).type == :directory end)
-    files = Enum.filter(list, fn(x) -> File.stat!(Path.join(store,x)).type != :directory end)
-    IO.inspect dirs, label: "Directory:"
-    IO.inspect files, label: "Files:"
+  defmacro __using__(_) do
+    quote do
+      alias ThetaMedia.Base
+      alias ThetaMedia.Dir
+    end
   end
-
 end
