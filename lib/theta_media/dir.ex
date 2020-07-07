287ff3e0f9aff8335ffc9038450275bf662baf16
Lang Pham <phamxuanlang@gmail.com>
2020-07-07 14:15:51 +0700

    Edit ls(base)

--- lib/theta_media/dir.ex
+++ lib/theta_media/dir.ex
@@ -21,7 +21,7 @@ defmodule ThetaMedia.Dir do
 		base = put_in(base.dirs, dirs)
 		files = Enum.filter(list, fn (x) -> File.stat!(Path.join(store, x)).type != :directory end)
 		base = put_in(base.files, files)
-		list = Path.split(base)
+		list = Path.split(store)
 		list_url = Enum.drop(list, base.root-1)
 		url = Path.join(list_url)
 		base = put_in(base.url, url)
